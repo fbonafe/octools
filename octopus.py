c8df0c7550377e6001b1226cd96502180a6913d2
Franco Bonafé <francobonafe@gmail.com>
2015-11-30 19:48:28 -0300

    Function loadData to get all data at once, fixed unsorted glob issue with Force function

--- octopus.py
+++ octopus.py
@@ -5,50 +5,113 @@ from scipy import interpolate
 
 hbar = 0.658
 
+class dynamics:
+    def __init__(self,time,field,dipole,forces,torque,alfa,omega,theta,charge,energy,mudot,muddot,torquepersite,muabs,muphi):
+        self.time = time
+        self.field = field
+        self.mu = dipole
+        self.forces = forces
+        self.torque = torque
+        self.alfa = alfa
+        self.omega = omega
+        self.theta = theta
+        self.charge = charge
+        self.energy = energy
+        self.mudot = mudot
+        self.muddot = muddot
+        self.torquepersite = torquepersite
+        self.muabs = muabs
+        self.muphi = muphi
+
+def Interpol(datax,datay,npoints):
+    f1 = interpolate.interp1d(datax,datay,kind='linear')
+    xnew = np.linspace(datax.min(),datax.max(),npoints)
+    ynew = f1(xnew)
+    return xnew,ynew
+
+
+# This loads all the relevant data
+def loadData(out_every):
+    
+    time,field,mu,charge = DipoleFieldVec()
+    dt = time[1]-time[0]
+    npoints = time.shape[0]
+    
+    force,torque,torquePerSite,alfa = Forces(time,dt*out_every)
+    
+    muabs = np.sqrt(mu[0]**2+mu[1]**2+mu[2]**2)
+    
+    muphi = np.angle(mu[0]+mu[1]*1j)
+    #muphi = np.array([x+2.*np.pi for x in muphi if x < 0])
+    for i in range(1,muphi.shape[0]):
+        if muphi[i] < muphi[i-1]:
+            muphi[i:] = 2*np.pi + muphi[i:]
+        
+    mudot = MuDot(time,muabs)
+    muddot = MuDDot(time,muabs)
+    omega,theta = AlphaOmegaTheta(time,alfa)
+    energy = Energy()
+    
+    return dynamics(time,field,mu,force,torque,alfa,omega,theta,charge,energy,mudot,muddot,torquePerSite,muabs,muphi)
+   
+
+#################################################################
+# This quantities are NOT automatically calculated by loadData,
+# should be used separately.
+
+def Epot(efield,mu):
+    epot = mu[0]*efield[0]+mu[1]*efield[1]+mu[2]*efield[2]
+    return epot
+
+def TorqueNuc(efield,mu):
+    torque = efield[0]*mu[1]-mu[0]*efield[1]
+    return torque
+#################################################################
+
+def Energy():
+    path = './td.general/'
+    e = []
+    with open(path+'energy','r') as laser:
+        for line in laser:
+            sl = line.strip()
+            if not sl.startswith("#"):
+                l = line.split()
+                e.append(float(l[2]))
+    e = np.array(e)
+    return e
+
 #Dipole Moment
 def DipoleFieldVec():
     path = './td.general/'
-    ex = ey = ez = mux = muy = muz = time = []
+    ex, ey, ez, mux, muy, muz, time, charge = [], [], [], [], [], [], [], []
     
     with open(path+'multipoles','r') as multipoles:
         for line in multipoles:
             sl = line.strip()
             if not sl.startswith("#"):
                 l = line.split()
-                mux.append(l[2])
-                muy.append(l[3])
-                muz.append(l[4])
+                time.append(float(l[1]))
+                charge.append(float(l[2]))
+                mux.append(float(l[3]))
+                muy.append(float(l[4]))
+                muz.append(float(l[5]))
                     
     with open(path+'laser','r') as laser:
         for line in laser:
             sl = line.strip()
             if not sl.startswith("#"):
                 l = line.split()
-                time.append(l[1])
-                ex.append(l[2])
-                ey.append(l[3])
-                ez.append(l[4])
+                ex.append(float(l[2]))
+                ey.append(float(l[3]))
+                ez.append(float(l[4]))
     
-    time = np.array(time[:len(mux)])
-    efield = np.array(ex[:len(mux)],ey[:len(mux)],ez[:len(mux)])
-    mu = np.array(mux,muy,muz)
+    time = np.array(time)*hbar
+    charge = np.array(charge)
+    efield = np.array([ex[:len(time)],ey[:len(time)],ez[:len(time)]])
+    mu = np.array([mux,muy,muz])
     
-    return time,efield,mu
+    return time,efield,mu,charge
 
-
-def Epot(efield,mu):
-    epot = mu[1]*efield[1]+mu[2]*efield[2]+mu[3]*efield[3]
-    return epot
-
-def TorqueNuc(efield,mu):
-    torque = efield[1]*mu[2]-mu[1]*efield[2]
-    return torque
-    
-def Interpol(datax,datay,npoints):
-    f1 = interpolate.interp1d(datax,datay,kind='linear')
-    xnew = np.linspace(datax.min(),datax.max(),npoints)
-    ynew = f1(xnew)
-    return xnew,ynew
 
 #DipoleMomentDervivative
 
@@ -69,27 +132,27 @@ def MuDDot(time,mu):
     
 #Force
 
-def Forces():
-    dt = 0.001
-    forcefiles = glob.glob('td.0*/forces.xsf')
+def Forces(realtime,dt): # dt in hbar/eV
+    forcefiles = sorted(glob.glob('td.0*/forces.xsf')) # VERY IMPORTANT TO INCLUDE sorted
     natoms = sum(1 for line in open('td.0000000/forces.xsf')) - 1
-    el = idn = x = y = z = fx0 = fy0 = fz0 = fx = fy = fz = torque = []*natoms
+    el, idn, x, y, z, fx0, fy0, fz0, fx, fy, fz, torque = [[None]*natoms for i in range(12)]
+    nsteps = len(forcefiles)
     
-    forcePerStep = torquePerStep = time = np.zeros((len(forcefiles)))
-    torquePerSite = np.zeros((len(forcefiles),natoms))
+    forcePerStep, torquePerStep, time = np.zeros((nsteps)), np.zeros((nsteps)), np.zeros((nsteps))
+    torquePerSite = np.zeros((nsteps,natoms))
     
 
     with open(forcefiles[0],'r+') as f:
         f.readline()
         for j,line in enumerate(f):
-            l = f.readline().split()
+            l = line.split()
             el[j] = l[0]
             if el[j] == 'C':
                 idn[j] = 6
             elif el[j] == 'H':
-                idn[j] == 1
+                idn[j] = 1
             elif el[j] == 'Na':
-                idn[j] == 9
+                idn[j] = 9
             x[j] = float(l[1])
             y[j] = float(l[2])
             z[j] = float(l[3])
@@ -99,22 +162,22 @@ def Forces():
     
     # Create AXSF file to be read with XCRYSDEN
     g = open('netforces.axsf','w')
-    g.write('ANIMSTEPS %i \n' & len(forcefiles))
+    g.write('ANIMSTEPS %i \n' % len(forcefiles))
 
     for i,ffile in enumerate(forcefiles):
         time[i] = i*dt
-        g.write('ATOMS %i \n' & i)
+        g.write('ATOMS %i \n' % (i+1))
         with open(ffile,'r+') as f:
             f.readline()
             rForce = [0.,0.,0.]
             totalTorque = 0.
             for j,line in enumerate(f):
-                l = f.readline().split()
+                l = line.split()
                 fx[j] = float(l[4]) - fx0[j]
                 fy[j] = float(l[5]) - fy0[j]
                 fz[j] = float(l[6]) - fz0[j]   
                 # Write to AXSF file
-                g.write("%i %10.6 %10.6 %10.6    %10.6 %10.6 %10.6" % (idn[j],x[j],y[j],z[j],fx[j],fy[j],fz[j]))
+                g.write("%i %10.6f %10.6f %10.6f %10.6f %10.6f %10.6f \n" % (idn[j],x[j],y[j],z[j],fx[j],fy[j],fz[j]))
                 # Calculate net force
                 rForce += [fx[j],fy[j],fz[j]]
                 # Calculate torque
@@ -125,15 +188,22 @@ def Forces():
             totalForce = np.sqrt(rForce[1]**2+rForce[2]**2+rForce[3]**2)
             forcePerStep[i] = totalForce
             torquePerStep[i] = totalTorque
-    
-    mInertia = momentInertia(natoms,el,x,y)
-    angAcceleration = torquePerStep/mInertia
-    
+            
     g.close()
+    mInertia = momentInertia(natoms,el,x,y)
     
-    return time,forcePerStep,torquePerStep,torquePerSite,angAcceleration
+    npoints = realtime.shape[0]
+    time2,force = Interpol(time,forcePerStep,npoints)
+    time2,torque = Interpol(time,torquePerStep,npoints)
+    alfa = torque/mInertia
     
-def AlfaOmegaTheta(time,torque,alfa): #$\alpha$ aceleracion angular
+    torquePerSite2 = np.zeros((npoints,natoms))
+    for i in range(natoms):
+        timenew,torquePerSite2[:,i] = Interpol(time,torquePerSite[:,i],npoints)
+    
+    return force,torque,torquePerSite,alfa
+    
+def AlphaOmegaTheta(time,alfa): #$\alpha$ aceleracion angular
     def integ(x, tck, constant=0):
         x = np.atleast_1d(x)
         out = np.zeros(x.shape[0], dtype=x.dtype)
@@ -142,20 +212,12 @@ def AlfaOmegaTheta(time,torque,alfa): #$\alpha$ aceleracion angular
     #    out += constant
         return out
 
-    npoints=4000
-
-    timenew,torquenew = Interpol(time,torque,npoints)
     tck = interpolate.splrep(time,alfa,s=0)
     omega = integ(time,tck)   # r'$\omega$ velocidad angular'
     
-    tck2 = interpolate.splrep(timetor,torqueint,s=0)
+    tck2 = interpolate.splrep(time,omega,s=0)
     theta = integ(time,tck2) # $\theta$ angulo de rotacion
-
-    time = time*hbar
-    
-    return time,alfa,omega,theta
-
-
+    return omega,theta
 
 def momentInertia(natoms,name,x,y):
     mass = [1.00794, 12.0107, 22.99]
